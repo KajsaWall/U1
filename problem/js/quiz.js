f52b33b2e6c8c02c02aaaa42fcb8bf1988694e84
KajsaWall <112626806+KajsaWall@users.noreply.github.com>
2023-02-18 00:13:43 +0100

    Random breed and image!
    Used the breeds array and made a random index function to choose a random breed. Fetched from the dog ceo zine api a picture of that breed.

--- problem/js/quiz.js
+++ problem/js/quiz.js
@@ -22,13 +22,28 @@ function quiz_setup (user_data) {
     `;
     document.querySelector("#logout").addEventListener("click", login_setup);
 
+    const answer_feedback = document.createElement("div");
+    answer_feedback.setAttribute("id", "answer_feedback");
+    answer_feedback.classList.add("hide");
+    answer_feedback.innerHTML = `<p></p> <button id="answer_button"></button>`
+    document.querySelector("body").append(answer_feedback);
+
+    quiz_start();
 }
 
-function quiz_load () {
+async function quiz_start() {
+    
+    const breed = ALL_BREEDS[random_number(ALL_BREEDS.length)];
+    const breed_request = new Request (`https://dog.ceo/api/breed/${breed.url}/images`);
+    const response = await fetch_function(breed_request);
+    const resource = await response.json();
+    const image = resource.message[0];
+    console.log(image);
 
+    document.querySelector(".image").style.backgroundImage = `url("${image}")`;
 }
-
 
 function random_number(max) {
-    return Math.floor((Math.random() * max)+1);
-}
+    // Returnerar en random siffra mellan 0 och max - 1
+    return Math.floor(max * Math.random());
+  }
